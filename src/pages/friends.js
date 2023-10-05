963944cbcf10c159fd867b9c6f93227128c55db8
noahwaseaten <kamen.mitsev@outlook.com>
2023-10-05 14:52:04 +0300

    Revert "added discord button & fixed <a> element size"
    This reverts commit b237510104a1d8a1059960591d72098712106460.

--- src/pages/friends.js
+++ src/pages/friends.js
@@ -4,8 +4,7 @@ export const friendsTemplate = () => `
     <div class="secret-div" load-page="memes"></div>
     <div class="flex">
         <p class="p-welcome">Phantom: super cool and chill dude, helped redesign and create the whole website</p>
-        <a href="https://www.twitch.tv/ph4nt0mk" target="_blank" style="height: 0px;"><img src="images/twitch.png" class="icon"></a>
-        <a href="https://discord.gg/AfnjAqpBaj" target="_blank" style="height: 0px;"><img src="images/discord.png" class="icon"></a>
+        <a href="https://www.twitch.tv/ph4nt0mk" target="_blank"><img src="images/twitch.png" class="twitch-logo"></a>
     </div>
     <p class="p-welcome">Kayden: lorem ipsum</p>
     <p class="p-welcome">Kalonzi: lorem ipsum</p>
