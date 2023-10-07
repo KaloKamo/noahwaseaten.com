421c3d8b582c9bd765cf4a45283d9be82bc0d503
noahwaseaten <kamen.mitsev@outlook.com>
2023-10-07 12:51:00 +0300

    added secret friends info

--- src/pages/secret-page.js
+++ src/pages/secret-page.js
@@ -1,6 +1,7 @@
 export const secretPageTemplate = () => `
     <h1><span class="first-letter">S</span>ECRET <a class="friends-page" load-page="friends">FRIENDS</a> PAGE</h1>
-    <p class="p-welcome">Phantom: loves to cry</p>
-    <p class="p-welcome">Kayden: hates bananas</p>
-    <p class="p-welcome">Kalonzi: league</p>
+    <p class="p-welcome">Phantom: "I like my women like I like my spaghetti - hot and in my mouth"</p>
+    <p class="p-welcome">Kayden: "in the search for quotes"</p>
+    <p class="p-welcome">Kalonzi: "in the search for quotes"</p>
+    <p class="p-welcome">Cat Ramsay: "Omg send me YouTube shorts once more and ill destroy your ass" ..?!?!</p>
 `;
