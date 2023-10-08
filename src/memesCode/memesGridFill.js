6edf1cb24d67f382aca061f99bf9efe8dfe34e7e
PH4NT0MK <ph4nt0mk1llr@gmail.com>
2023-10-08 20:11:21 +0300

    Fixed memesGridFill

--- src/memesCode/memesGridFill.js
+++ src/memesCode/memesGridFill.js
@@ -2,7 +2,7 @@ export const MemesGridFill = () => {
     let answer = '';
 
     for(let i = 0; i < 705; i++) { // 47 on row x 15 rows
-        answer += '<div class="meme-grid-box"></div>';
+        answer += '<div class="meme-grid-box-full"><div class="meme-grid-box"></div></div>';
     }
 
     return answer;
