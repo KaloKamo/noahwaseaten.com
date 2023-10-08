e6a762c1a3080e19a97e5b4fa753ffdf11ac6c9e
PH4NT0MK <ph4nt0mk1llr@gmail.com>
2023-10-09 02:22:48 +0300

    Cleaned up style.css

--- src/pages/friends.js
+++ src/pages/friends.js
@@ -2,7 +2,7 @@ export const friendsTemplate = () => `
     <h1><span class="first-letter">F</span>RIENDS</h1>
     <div class="secret-div" load-page="secret-page"></div>
     <div class="secret-div" load-page="memes"></div>
-    <div class="flex">
+    <div class="friend-div">
         <p class="p-welcome">Phantom: super cool and chill dude, helped redesign and create the whole website</p>
         <a href="https://www.twitch.tv/ph4nt0mk" target="_blank"><img src="images/twitch.png" class="twitch-logo"></a>
         <a href="https://discord.gg/AfnjAqpBaj" target="_blank"><img src="images/discord.png" class="discord-logo"></a>
--- src/pages/memes.js
+++ src/pages/memes.js
@@ -1,5 +1,5 @@
 export const memesTemplate = () => `
-    <h1><span class="first-letter">W</span>ELCOME TO</h1>
-    <h1 class="meme-title">THE MEMES PAGE!</h1>
+    <h1><span class="first-letter">W</span>ELCOME TO
+    THE MEMES PAGE!</h1>
     <div id="memes-grid-slot-transitions"></div>
 `;
