43ccc427e5993fb0351c15bbafa0a9f4351f10c9
noahwaseaten <kamen.mitsev@outlook.com>
2023-10-10 02:34:04 +0300

    same thing

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,11 @@
 export const homeTemplate = () => `
 <div id="container">
     <h1 class="hi-noah">Hi, I'm <span class="first-letter">N</span>OAH</h1>
-    <p>im currently learning to code, as of writing this im only working on this website which is the basic web development languages (html, css and js)    </p>
-    <p> <br> <br> <br> <br> check out my socials!
-    <br> <a href="https://www.youtube.com/channel/noahwaseaten" target="_blank"><img src="images/discord.png" class="youtube-logo1"></a>
-    <br> <a href="https://www.twitch.tv/noahwaseaten" target="_blank"><img src="images/twitch.png" class="twitch-logo1"></a>
-    <br> <a href="https://github.com/noahwaseaten" target="_blank"><img src="images/github.png" class="github-logo1"></a>
+    <p>im currently learning to code, as of writing this im working only on this website which contains the basic web development languages (html, css and js), no fancy frameworks like Astro, Vue.js or Tailwind css   </p>
+    <p> <br> <br> <br> <br> <br> <br>  check out my socials!
+    <br> <a href="https://youtube.noahwaseaten.com" target="_blank"><img src="images/youtube.png" class="youtube-logo1"></a>
+    <br> <a href="https://twitch.noahwaseaten.com" target="_blank"><img src="images/twitch.png" class="twitch-logo1"></a>
+    <br> <a href="https://github.noahwaseaten.com" target="_blank"><img src="images/github.png" class="github-logo1"></a>
     <br> <a href="https://discord.noahwaseaten.com" target="_blank"><img src="images/discord.png" class="discord-logo1"></a>
     </div>
 `;
