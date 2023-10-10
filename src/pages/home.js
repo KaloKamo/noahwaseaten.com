39e1db10f3c589cf7cf7a291f901ca5ebdbd027c
noahwaseaten <kamen.mitsev@outlook.com>
2023-10-10 10:36:03 +0300

    tried fixing footer, couldnt again

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,9 +3,9 @@ export const homeTemplate = () => `
     <h1 class="hi-noah">Hi, I'm <span class="first-letter">N</span>OAH</h1>
     <p>im currently learning to code, as of writing this im working only on this website which contains the basic web development languages (html, css and js), no fancy frameworks like Astro, Vue.js or Tailwind css   </p>
     <p> <br> <br> <br> <br> <br> <br>  check out my socials!
-    <br> <a href="https://youtube.noahwaseaten.com" target="_blank"><img src="images/youtube.png" class="youtube-logo1"></a>
-    <br> <a href="https://twitch.noahwaseaten.com" target="_blank"><img src="images/twitch.png" class="twitch-logo1"></a>
-    <br> <a href="https://github.noahwaseaten.com" target="_blank"><img src="images/github.png" class="github-logo1"></a>
-    <br> <a href="https://discord.noahwaseaten.com" target="_blank"><img src="images/discord.png" class="discord-logo1"></a>
+    <br> <a href="https://youtube.noahwaseaten.com" target="_blank"><img src="images/youtube.png" class="youtube-logo1"></a> 
+    <br> <a href="https://twitch.noahwaseaten.com" target="_blank"><img src="images/twitch.png" class="twitch-logo1"></a> 
+    <br> <a href="https://github.noahwaseaten.com" target="_blank"><img src="images/github.png" class="github-logo1"></a> 
+    <br> <a href="https://discord.noahwaseaten.com" target="_blank"><img src="images/discord.png" class="discord-logo1"></a> 
     </div>
 `;
