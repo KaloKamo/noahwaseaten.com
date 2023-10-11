0a7498f44365214d604525c2870f63d80a8e1b73
noahwaseaten <kamen.mitsev@outlook.com>
2023-10-11 03:01:08 +0300

    added text next to social icons on home, added lorem ipsum to contacts and downloads until we get them removed

--- src/pages/friends.js
+++ src/pages/friends.js
@@ -1,5 +1,6 @@
 export const friendsTemplate = () => `
     <h1><span class="first-letter">F</span>RIENDS</h1>
+    
     <div class="secret-div" load-page="secret-page"></div>
     <div class="secret-div" load-page="memes"></div>
     <div class="friend-div">
@@ -8,15 +9,8 @@ export const friendsTemplate = () => `
         <a href="https://discord.gg/AfnjAqpBaj" target="_blank"><img src="images/discord.png" class="discord-logo"></a>
         <a href="https://github.com/ph4nt0mk" target="_blank"><img src="images/github.png" class="github-logo"></a>
     </div>
-    <p>Kayden: lorem ipsum</p>
-    <p>Kalonzi: lorem ipsum</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fermentum, tellus fringilla sagittis laoreet, sem erat consequat purus, ac laoreet enim nisi at nisi. Proin quis scelerisque justo. Vivamus ultrices varius dolor. Vivamus blandit pellentesque mauris sit amet tincidunt. Nulla pulvinar dapibus erat quis hendrerit. Integer elit tellus, fermentum eget ligula ac, tincidunt pellentesque dui. Maecenas diam metus, pellentesque nec neque et, tincidunt rutrum lectus. Donec ac vulputate tellus. Donec euismod sollicitudin ipsum, sit amet blandit lectus luctus eget. Aliquam auctor elit sed urna pellentesque, sit amet feugiat lorem tempus. Aliquam sollicitudin neque turpis, vel euismod odio consequat at. Ut dui eros, accumsan sit amet viverra at, finibus sit amet nibh. Nunc lobortis nunc a volutpat vulputate.
-    
-    Pellentesque in porttitor purus, sed finibus sem. Aliquam egestas, nunc et consequat euismod, metus nisl elementum turpis, sit amet suscipit mauris ex nec quam. Praesent lorem arcu, sagittis ornare mollis non, rhoncus a arcu. Quisque bibendum quam molestie ligula feugiat, vel cursus sem aliquet. Sed ornare at enim pharetra interdum. Nam posuere cursus orci a vulputate. Morbi blandit sapien quis vestibulum tincidunt. Integer lacinia risus maximus bibendum varius. Nulla id ante egestas, placerat lorem a, egestas nulla. Nulla pellentesque justo non erat consectetur finibus.
-    
-    Phasellus sit amet sagittis ipsum. Integer dignissim arcu ut nunc tempus, in feugiat augue pretium. Nunc vitae porttitor est, quis mattis lectus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum convallis velit rutrum, feugiat nibh ut, dapibus nisl. Ut id convallis est, vitae posuere est. Quisque mollis ligula non sagittis condimentum. Aenean suscipit ac quam sit amet hendrerit. Donec ultricies odio nec tincidunt imperdiet. Mauris vel sapien congue, gravida nisl at, tristique tellus.
-    
-    Aenean nunc metus, pulvinar in ex a, congue dignissim velit. Vivamus vehicula volutpat efficitur. Morbi finibus augue sit amet feugiat mattis. Praesent luctus elit sed diam tincidunt eleifend. Quisque rutrum nunc eget nisl auctor tempus. Cras sagittis risus nec nisi mattis vehicula. Quisque ut nunc a erat viverra fermentum.
-    
-    Donec lacinia lobortis nisl, interdum cursus dui ullamcorper nec. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut eget magna odio. Integer id mauris facilisis, gravida tortor quis, pharetra neque. Aliquam hendrerit vulputate tortor, vitae accumsan purus rutrum in. Nullam vel felis at risus condimentum elementum non vitae mi. Cras sed sapien at ex sagittis pulvinar. Mauris vulputate lectus eu sem convallis, et ultricies augue sollicitudin. Curabitur vitae nisi ante. Maecenas efficitur diam et nisl porttitor placerat. Nullam maximus odio nisl. Duis aliquet lacus nec eros iaculis vestibulum. Quisque vehicula semper accumsan. Ut sit amet ante vitae urna scelerisque hendrerit dignissim a eros. Nullam nec nibh elit.</p>
+    <p>Kayden: he can be annoying sometimes, but thats only because you forced him to, is the one that will probably get cancelled the second they get send a post in twitter</p>
+    <br>
+    <p>Kalonzi: he plays league of legends, but is also mentally fine. a person you'll never get to know again if you lose them</p>
+
 `;
