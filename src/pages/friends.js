b795c457e2e4915927c2b754da3a31f11cc7a592
noahwaseaten <kamen.mitsev@outlook.com>
2023-10-16 00:19:18 +0300

    Revert "ARG"
    This reverts commit 380b95c5a060ed2f245cb583b7705bc23d29a070.

--- src/pages/friends.js
+++ src/pages/friends.js
@@ -6,11 +6,11 @@ export const friendsTemplate = () => `
     <div class="friend-div">
         <p class="p-welcome">Phantom: super cool and chill dude, helped redesign and create the whole website</p>
         <a href="https://www.twitch.tv/ph4nt0mk" target="_blank"><img src="images/twitch.png" class="twitch-logo"></a>
-        <a href="https://discord.gg/BNA8Prcg" target="_blank"><img src="images/discord.png" class="discord-logo"></a>
+        <a href="https://discord.gg/AfnjAqpBaj" target="_blank"><img src="images/discord.png" class="discord-logo"></a>
         <a href="https://github.com/ph4nt0mk" target="_blank"><img src="images/github.png" class="github-logo"></a>
     </div>
     <p>Kayden: he can be annoying sometimes, but thats only because you forced him to, is the one that will probably get cancelled the second they get send a post in twitter</p>
     <br>
-    <p>Kalonzi: he plays league of legends, but is also mentally fine. Impressive</p>
+    <p>Kalonzi: he plays league of legends, but is also mentally fine. a person you'll never get to know again if you lose them</p>
 
 `;
