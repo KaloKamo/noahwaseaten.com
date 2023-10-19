68aaed8c60ba80e9f20a4e519be7a072b40e0f1e
PH4NT0MK <ph4nt0mk1llr@gmail.com>
2023-10-19 17:01:10 +0300

    Added three more games

--- src/data/gamesData.js
+++ src/data/gamesData.js
@@ -1,86 +1,58 @@
 export const gamesData = () => [
     {
         id: 1,
-        name: "Minecraft",
-        cover: "/src/data/gamesImages/minecraft.png",
-        releaseYear: "2009",
-        releaseDate: "May 17"
+        name: 'Minecraft',
+        cover: '/src/data/gamesImages/minecraft.png',
+        releaseYear: '2009',
+        releaseDate: 'May 17',
+        link: 'https://www.minecraft.net/en-us'
     },
     {
         id: 2,
-        name: "Factorio",
-        cover: "/src/data/gamesImages/factorio.png",
-        releaseYear: "2016",
-        releaseDate: "February 25"
+        name: 'Factorio',
+        cover: '/src/data/gamesImages/factorio.png',
+        releaseYear: '2016',
+        releaseDate: 'February 25',
+        link: 'https://store.steampowered.com/app/427520/Factorio/'
     },
     {
         id: 3,
-        name: "Valorant",
-        cover: "/src/data/gamesImages/valorant.png",
-        releaseYear: "2020",
-        releaseDate: "June 2"
+        name: 'Valorant',
+        cover: '/src/data/gamesImages/valorant.png',
+        releaseYear: '2020',
+        releaseDate: 'June 2',
+        link: 'https://playvalorant.com/en-gb/'
     },
     {
         id: 4,
-        name: "CS2",
-        cover: "/src/data/gamesImages/cs2.png",
-        releaseYear: "2023",
-        releaseDate: "September 27"
+        name: 'CS2',
+        cover: '/src/data/gamesImages/cs2.png',
+        releaseYear: '2023',
+        releaseDate: 'September 27',
+        link: 'https://store.steampowered.com/app/730/CounterStrike_2/'
     },
     {
-        id: 4,
-        name: "CS2",
-        cover: "/src/data/gamesImages/cs2.png",
-        releaseYear: "2023",
-        releaseDate: "September 27"
+        id: 5,
+        name: 'Shoppe Keep 2',
+        cover: '/src/data/gamesImages/shoppeKeep2.png',
+        releaseYear: '2019 ',
+        releaseDate: 'Apr 25',
+        link: 'https://store.steampowered.com/app/684580/Shoppe_Keep_2/'
     },
     {
-        id: 4,
-        name: "CS2",
-        cover: "/src/data/gamesImages/cs2.png",
-        releaseYear: "2023",
-        releaseDate: "September 27"
+        id: 6,
+        name: 'Anno 1404',
+        cover: '/src/data/gamesImages/anno1404.png',
+        releaseYear: '2006',
+        releaseDate: 'October 26',
+        link: 'https://store.steampowered.com/app/1281630/Anno_1404__History_Edition/'
     },
     {
-        id: 4,
-        name: "CS2",
-        cover: "/src/data/gamesImages/cs2.png",
-        releaseYear: "2023",
-        releaseDate: "September 27"
-    },
-    {
-        id: 4,
-        name: "CS2",
-        cover: "/src/data/gamesImages/cs2.png",
-        releaseYear: "2023",
-        releaseDate: "September 27"
-    },
-    {
-        id: 4,
-        name: "CS2",
-        cover: "/src/data/gamesImages/cs2.png",
-        releaseYear: "2023",
-        releaseDate: "September 27"
-    },
-    {
-        id: 4,
-        name: "CS2",
-        cover: "/src/data/gamesImages/cs2.png",
-        releaseYear: "2023",
-        releaseDate: "September 27"
-    },
-    {
-        id: 4,
-        name: "CS2",
-        cover: "/src/data/gamesImages/cs2.png",
-        releaseYear: "2023",
-        releaseDate: "September 27"
-    },
-    {
-        id: 4,
-        name: "CS2",
-        cover: "/src/data/gamesImages/cs2.png",
-        releaseYear: "2023",
-        releaseDate: "September 27"
+        id: 7,
+        name: 'Hollow Knight',
+        cover: '/src/data/gamesImages/hollowKnight.png',
+        releaseYear: '2017',
+        releaseDate: 'February 24',
+        link: 'https://store.steampowered.com/app/367520/Hollow_Knight/'
     },
 ]
