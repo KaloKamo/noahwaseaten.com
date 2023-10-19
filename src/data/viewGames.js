345d2192c8c201899d84cb427d41f2c1e907b725
PH4NT0MK <ph4nt0mk1llr@gmail.com>
2023-10-19 17:38:09 +0300

    Started implementing game-details - #12

--- src/data/viewGames.js
+++ src/data/viewGames.js
@@ -5,14 +5,22 @@ export const viewGames = () => {
 
     gamesData().map(game => {
         gamesHTML += `
-        <div class="game-div">
+        <div class="game-div" view-details="${game}">
             <h2>${game.name}</h2>
-            <p>${game.releaseYear}</p>
             <img src="${game.cover}" alt="${game.name}.png">
-            <p class="view-details">View Details</p>
         </div>\n
         `;
     });
 
     return gamesHTML;
 }
+
+export const gameInfo = (game) => `
+    <h1>${game.name}</h1>
+    <div class="game-info">
+        <img src="${game.cover}">
+        <p>Release Year: ${game.releaseYear}</p>
+        <p>Release Date: ${game.releaseDate}</p>
+        <p>Game Link: ${game.link}</p>
+    </div>
+`;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import { loadPage } from "./navigation-events.js";
+import { loadPage } from './navigation-events.js';
+import { gameInfo } from './data/viewGames.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    loadPage('home');
-});
 
-document.addEventListener('click', event => {
+    document.addEventListener('click', event => {
     
-    const headerPages = ['noah-logo', 'content-page', 'friends-page', 'games-page', 'contacts-page', 'secret-div'];
+        const headerPages = ['noah-logo', 'content-page', 'friends-page', 'games-page', 'contacts-page', 'secret-div'];
 
-    if(headerPages.some(page => event.target.classList.contains(page))) {
-        loadPage(event.target.getAttribute('load-page'));
-    }
+        if(headerPages.some(page => event.target.classList.contains(page))) {
+            loadPage(event.target.getAttribute('load-page'));
+        }
+    });
+
+    document.getElementsByClassName('game-div').addEventListener('click', (e) => {
+        gameInfo(e.target.getAttribute('view-details'));
+    });
+
+    loadPage('home');
 });
