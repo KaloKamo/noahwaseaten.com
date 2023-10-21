9d554cb6b279fa53916c952842f5fe72d1370467
PH4NT0MK <ph4nt0mk1llr@gmail.com>
2023-10-21 18:01:28 +0300

    Fixed releaseDate

--- src/data/gamesData.js
+++ src/data/gamesData.js
@@ -1,66 +1,58 @@
 export const gamesData = () => [
     {
         id: 1,
-        name: 'Minecraft',
+        title: 'Minecraft',
         cover: '/src/data/gamesImages/minecraft.png',
-        releaseYear: '2009',
-        releaseDate: 'May 17',
+        releaseDate: 'May 17, 2009',
         link: 'https://www.minecraft.net/en-us'
     },
     {
         id: 2,
-        name: 'Factorio',
+        title: 'Factorio',
         cover: '/src/data/gamesImages/factorio.png',
-        releaseYear: '2016',
-        releaseDate: 'February 25',
+        releaseDate: 'February 25, 2016',
         link: 'https://store.steampowered.com/app/427520/Factorio/'
     },
     {
         id: 3,
-        name: 'Valorant',
+        title: 'Valorant',
         cover: '/src/data/gamesImages/valorant.png',
-        releaseYear: '2020',
-        releaseDate: 'June 2',
+        releaseDate: 'June 2, 2020',
         link: 'https://playvalorant.com/en-gb/'
     },
     {
         id: 4,
-        name: 'CS2',
+        title: 'CS2',
         cover: '/src/data/gamesImages/cs2.png',
-        releaseYear: '2023',
-        releaseDate: 'September 27',
+        releaseDate: 'September 27, 2023',
         link: 'https://store.steampowered.com/app/730/CounterStrike_2/'
     },
     {
         id: 5,
-        name: 'Shoppe Keep 2',
+        title: 'Shoppe Keep 2',
         cover: '/src/data/gamesImages/shoppeKeep2.png',
-        releaseYear: '2019 ',
-        releaseDate: 'Apr 25',
+        releaseDate: 'Apr 25, 2019',
         link: 'https://store.steampowered.com/app/684580/Shoppe_Keep_2/'
     },
     {
         id: 6,
-        name: 'Anno 1404',
+        title: 'Anno 1404',
         cover: '/src/data/gamesImages/anno1404.png',
-        releaseYear: '2006',
-        releaseDate: 'October 26',
+        releaseDate: 'October 26, 2006',
         link: 'https://store.steampowered.com/app/1281630/Anno_1404__History_Edition/'
     },
     {
         id: 7,
-        name: 'Hollow Knight',
+        title: 'Hollow Knight',
         cover: '/src/data/gamesImages/hollowKnight.png',
-        releaseYear: '2017',
-        releaseDate: 'February 24',
+        releaseDate: 'February 24, 2017',
         link: 'https://store.steampowered.com/app/367520/Hollow_Knight/'
     },
     {
         id: 8,
-        name: 'Fortnite',
+        title: 'Fortnite',
         cover: '/src/data/gamesImages/fortnite.png',
-        releaseYear: '2017',
-        releaseDate: 'July 21',
+        releaseDate: 'July 21, 2017',
         link: 'https://www.fortnite.com/'
     },
 ]
