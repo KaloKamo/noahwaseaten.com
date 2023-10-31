f36bbbe6ca555b9bee12c6e40ebbde4f1b713654
noahwaseaten <kamen.mitsev@outlook.com>
2023-11-01 00:22:22 +0200

    redesign website message

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,7 @@
 export const homeTemplate = () => `
 <div id="container">
     <h1 class="hi-noah">Hi, I'm <span class="first-letter">N</span>OAH</h1>
-    <p>im currently learning to code, as of writing this im working only on this website which contains the basic web development languages (html, css and js), no fancy frameworks like Astro, Vue.js or Tailwind css   </p>
+    <p>I will be redesigning this thing, You can still look around but dont expect much   </p>
     <p> <br> <br> <br> <br> <br>  check out my socials!
     <br> <a href="https://youtube.noahwaseaten.com" target="_blank"><img src="images/youtube.png" class="youtube-logo1"> <span class="logo-text"> YouTube </span> </a>
     <br> <a href="https://twitch.noahwaseaten.com" target="_blank"><img src="images/twitch.png" class="twitch-logo1"> <span class="logo-text"> Twitch </span> </a> 
