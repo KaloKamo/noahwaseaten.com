9ab70561ac2448d41233666ec11e1c2f451744f5
noahwaseaten <kamen.mitsev@outlook.com>
2023-11-26 14:45:49 +0200

    new home message

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,7 @@
 export const homeTemplate = () => `
 <div id="container">
     <h1 class="hi-noah">Hi, I'm <span class="first-letter">N</span>OAH</h1>
-    <p>I will be redesigning this thing, You can still look around but dont expect much   </p>
+    <p>This will soon be a library for projects im working on/worked on. You can look around until i change it.   </p>
     <p> <br> <br> <br> <br> <br>  check out my socials!
     <br> <a href="https://youtube.noahwaseaten.com" target="_blank"><img src="images/youtube.png" class="youtube-logo1"> <span class="logo-text"> YouTube </span> </a>
     <br> <a href="https://twitch.noahwaseaten.com" target="_blank"><img src="images/twitch.png" class="twitch-logo1"> <span class="logo-text"> Twitch </span> </a> 
