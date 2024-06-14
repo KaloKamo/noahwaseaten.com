a3d1eef558481bbc0faccc0754f96858d56d5f33
noahwaseaten <kamen.mitsev@outlook.com>
2024-06-14 20:33:21 +0300

    second try

--- contacts.js
+++
@@ -1,4 +0,0 @@
-export const contactsTemplate = () => `
-    <h1><span class="first-letter">C</span>ONTACTS</h1>
-    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-`;
--- downloads.js
+++
@@ -1,4 +0,0 @@
-export const downloadsTemplate = () => `
-<h1><span class="first-letter">P</span>rojects</h1>
-    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-`;
