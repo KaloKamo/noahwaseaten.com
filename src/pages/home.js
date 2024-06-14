eba918828b61f24a6cb9a90dcb73b17c4417d7d4
noahwaseaten <kamen.mitsev@outlook.com>
2024-06-14 20:30:02 +0300

    FIXED changes

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,6 @@
 export const homeTemplate = () => `
 <div id="container">
-    <h1 class="hi-noah">Hi, I'm <span class="first-letter">N</span>OAH</h1>
-    <p>This will soon be a library for projects im working on/worked on. You can still look around until i change it or tranfer the domain.   </p>
-    <p> <br> <br> <br> <br> <br>  check out my socials!
-    <br> <a href="https://youtube.noahwaseaten.com" target="_blank"><img src="images/youtube.png" class="youtube-logo1"> <span class="logo-text"> YouTube </span> </a>
-    <br> <a href="https://twitch.noahwaseaten.com" target="_blank"><img src="images/twitch.png" class="twitch-logo1"> <span class="logo-text"> Twitch </span> </a> 
-    <br> <a href="https://github.noahwaseaten.com" target="_blank"><img src="images/github.png" class="github-logo1"> <span class="logo-text"> Github </span> </a>
-    <br> <a href="https://discord.noahwaseaten.com" target="_blank"><img src="images/discord.png" class="discord-logo1"> <span class="logo-text"> Discord Server </span> </a> 
+    <h1 class="hi-noah">hi, I'm <span class="first-letter">n</span>oah</h1>
+    <p>this is my "portfolio", which will be deleted permanently and remade ENTIRELY with React soon </p>
     </div>
 `;
