493909465d633fb2059e103a1433c6095bbf3c6f
noahwaseaten <kamen.mitsev@outlook.com>
2024-06-15 20:49:33 +0300

    small change again

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,7 @@ export const homeTemplate = () => `
 <div id="container">
     <h1 class="hi-noah">hi, I'm <span class="first-letter">n</span>oah</h1>
     <p>this is my "portfolio", which will be deleted permanently and remade ENTIRELY with React and made mobile friendly soon</p>
-        <div class="githuber"> check out my <a href="https://www.twitch.tv/ph4nt0mk" target="_blank"> github </a> 
+        <div class="githuber"> check out my <a href="https://github.com/noahwaseaten" target="_blank"> github </a> 
         </div>
 </div>
 `;
