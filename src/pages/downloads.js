25c89140c2c84ef5939df741ab80839b258fd387
noahwaseaten <kamen.mitsev@outlook.com>
2024-06-15 15:54:40 +0300

    new button hehe

--- src/pages/downloads.js
+++ src/pages/downloads.js
@@ -1,4 +1,41 @@
 export const downloadsTemplate = () => `
 <h1><span class="first-letter">P</span>rojects</h1>
-    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+     <a href="https://github.com/KaloKamo/noahwaseaten.com" target="_blank" class="project-div">
+      <div class="brand-top">
+        <img src="images/munchcode-favicon-black-whitebg.svg" class="munch" />
+        <p class="munchcode-text">Munchcode</p>
+      </div>
+      <div class="brand-title">
+        <h3>noahwaseaten.com</h3>
+        <p class="desc">My personal website</p>
+      </div>
+      <div class="languages">
+        <span class="js">JavaScript</span>
+      </div>
+      <div class="open2">
+        <svg
+          role="img"
+          aria-hidden="true"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          fill="none"
+          class="open"
+        >
+          <title>Open Externally</title>
+          <path
+            d="M21 3h-6m6 0l-9 9m9-9v6"
+            stroke-width="2"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></path>
+          <path
+            d="M21 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V5a2 2 0 012-2h6"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+          ></path>
+        </svg>
+      </div>
+    </a>
 `;
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,5 +2,7 @@ export const homeTemplate = () => `
 <div id="container">
     <h1 class="hi-noah">hi, I'm <span class="first-letter">n</span>oah</h1>
     <p>this is my "portfolio", which will be deleted permanently and remade ENTIRELY with React soon </p>
-    </div>
+        <div class="githuber"> check out my <a href="https://www.twitch.tv/ph4nt0mk" target="_blank"> github </a> 
+        </div>
+</div>
 `;
