9625b30b383daeccfde73f1ffb4af76af5ef801b
noahwaseaten <kamen.mitsev@outlook.com>
2024-06-15 15:55:31 +0300

    small text change

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,7 @@
 export const homeTemplate = () => `
 <div id="container">
     <h1 class="hi-noah">hi, I'm <span class="first-letter">n</span>oah</h1>
-    <p>this is my "portfolio", which will be deleted permanently and remade ENTIRELY with React soon </p>
+    <p>this is my "portfolio", which will be deleted permanently and remade ENTIRELY with React and made mobile friendly soon</p>
         <div class="githuber"> check out my <a href="https://www.twitch.tv/ph4nt0mk" target="_blank"> github </a> 
         </div>
 </div>
