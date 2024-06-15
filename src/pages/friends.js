ecabd67a66ca4f12aec20bb2c20c2ba40979eeec
noahwaseaten <kamen.mitsev@outlook.com>
2024-06-15 23:12:05 +0300

    new friends look

--- src/pages/friends.js
+++ src/pages/friends.js
@@ -4,10 +4,20 @@ export const friendsTemplate = () => `
     <div class="secret-div" load-page="secret-page"></div>
     <div class="secret-div" load-page="memes"></div>
     <div class="friend-div">
-        <p class="p-welcome">Phantom: super cool and chill dude, helped create the website</p>
-        <a href="https://www.twitch.tv/ph4nt0mk" target="_blank"><img src="images/twitch.png" class="twitch-logo"></a>
-        <a href="https://discord.gg/AfnjAqpBaj" target="_blank"><img src="images/discord.png" class="discord-logo"></a>
-        <a href="https://github.com/ph4nt0mk" target="_blank"><img src="images/github.png" class="github-logo"></a>
+        <div class="flex">
+      <a target="_blank" href="https://github.com/PH4NT0MK" class="phan">
+        <img src="images/PFPNICE.png" class="phane" />
+      </a>
+      <a target="_blank" href="https://github.com/PH4NT0MK" class="phan">
+        <img src="images/PFPNICE.png" class="phane" />
+      </a>
+      <a target="_blank" href="https://github.com/PH4NT0MK" class="phan">
+        <img src="images/PFPNICE.png" class="phane" />
+      </a>
+      <a target="_blank" href="https://github.com/PH4NT0MK" class="phan">
+        <img src="images/PFPNICE.png" class="phane" />
+      </a>
+    </div>
     </div>
 
 `;
