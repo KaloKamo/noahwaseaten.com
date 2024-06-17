53c628a2ded8b110755e1e2d11273574e2b5cc63
noahwaseaten <kamen.mitsev@outlook.com>
2024-06-17 11:10:24 +0300

    secret

--- src/pages/secret-page.js
+++ src/pages/secret-page.js
@@ -2,4 +2,5 @@ export const secretPageTemplate = () => `
     <h1><span class="first-letter">S</span>ECRET FRIENDS PAGE</h1>
     <p class="p-welcome">Phantom: "I like my women like I like my spaghetti - hot and in my mouth"</p>
     <p class="p-welcome">Kayden: "people are like bananas, the black ones are bad"</p>
+    <p class="p-welcome">Cat Ramsay: "Lost Quote"</p>
 `;
