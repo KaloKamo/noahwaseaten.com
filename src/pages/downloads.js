f090eef70e28cc66cc24bd41dbe44e64833b6b2d
noahwaseaten <kamen.mitsev@outlook.com>
2024-06-18 00:56:42 +0300

    FIX MOBILE

--- src/pages/downloads.js
+++ src/pages/downloads.js
@@ -1,65 +1,126 @@
 export const downloadsTemplate = () => `
 <h1><span class="first-letter">P</span>rojects</h1>
-     <a href="https://github.com/KaloKamo/noahwaseaten.com" target="_blank" class="project-div">
-      <div class="brand-top">
-        <img src="images/munchcode-favicon-black-whitebg.svg" class="munch" />
-        <p class="munchcode-text">Kalmo</p>
-      </div>
-      <div class="brand-title">
-        <h3>noahwaseaten.com</h3>
-        <p class="desc">My personal website</p>
-      </div>
-      <div class="languages">
-        <span class="js">JavaScript</span>
-      </div>
-      <div class="open2">
-        <svg
-          role="img"
-          aria-hidden="true"
-          viewBox="0 0 24 24"
-          stroke-width="2"
-          fill="none"
-          class="open"
+    <div class="flexer">
+      <a
+        href="https://github.com/KaloKamo/noahwaseaten.com"
+        target="_blank"
+        class="project-div"
+      >
+        <div class="brand-top">
+          <img src="images/munchcode-favicon-black-whitebg.svg" class="munch" />
+          <p class="munchcode-text">Kalmo</p>
+        </div>
+        <div class="brand-title">
+          <h3>noahwaseaten.com</h3>
+          <p class="desc">My personal website</p>
+        </div>
+        <div class="languages">
+          <span class="js">JavaScript</span>
+        </div>
+        <div class="open2">
+          <svg
+            role="img"
+            aria-hidden="true"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            fill="none"
+            class="open"
+          >
+            <title>Open Externally</title>
+            <path
+              d="M21 3h-6m6 0l-9 9m9-9v6"
+              stroke-width="2"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+            <path
+              d="M21 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V5a2 2 0 012-2h6"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+            ></path>
+          </svg>
+        </div>
+      </a>
+      <a
+        href="https://github.com/Project-1-Gifs-SPA/Chatter"
+        target="_blank"
+        class="project-div3"
+      >
+        <div class="brand-top2">
+          <img src="images/munchcode-favicon-black-whitebg.svg" class="munch" />
+          <p class="munchcode-text2">
+            Aleksandar Spasov, Gergana Bogdanova, Kaloyan Todorov
+          </p>
+        </div>
+        <div class="brand-title2">
+          <h3>Chatter</h3>
+          <p class="desc">PH4NT0MK's third project</p>
+        </div>
+        <div class="languages2">
+          <span class="js">React.js</span>
+        </div>
+        <div class="open">
+          <svg
+            role="img"
+            aria-hidden="true"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            fill="none"
+            class="open2"
+          >
+            <title>Open Externally</title>
+            <path
+              d="M21 3h-6m6 0l-9 9m9-9v6"
+              stroke-width="2"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+            <path
+              d="M21 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V5a2 2 0 012-2h6"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+            ></path>
+          </svg>
+        </div>
+      </a>
+      <div class="discord">
+        <div class="footer"></div>
+        <span class="add2"
+          >you should consider <br />
+          joining my</span
         >
-          <title>Open Externally</title>
-          <path
-            d="M21 3h-6m6 0l-9 9m9-9v6"
-            stroke-width="2"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          ></path>
-          <path
-            d="M21 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V5a2 2 0 012-2h6"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-          ></path>
-        </svg>
-      </div>
-    </a>
-    <div class="discord">
-    <div class="footer"></div>
-      <span class="add2">you should consider <br> joining my</span>
-      <div class="discord-div2">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 127.14 96.36" class="clyde2">
-          <defs>
-            
-          </defs>
-          <g id="图层_2" data-name="图层 2">
-            <g id="Discord_Logos" data-name="Discord Logos">
-              <g
-                id="Discord_Logo_-_Large_-_White"
-                data-name="Discord Logo - Large - White"
-              >
-                <path
-                  class="cls-1"
-                  d="M107.7,8.07A105.15,105.15,0,0,0,81.47,0a72.06,72.06,0,0,0-3.36,6.83A97.68,97.68,0,0,0,49,6.83,72.37,72.37,0,0,0,45.64,0,105.89,105.89,0,0,0,19.39,8.09C2.79,32.65-1.71,56.6.54,80.21h0A105.73,105.73,0,0,0,32.71,96.36,77.7,77.7,0,0,0,39.6,85.25a68.42,68.42,0,0,1-10.85-5.18c.91-.66,1.8-1.34,2.66-2a75.57,75.57,0,0,0,64.32,0c.87.71,1.76,1.39,2.66,2a68.68,68.68,0,0,1-10.87,5.19,77,77,0,0,0,6.89,11.1A105.25,105.25,0,0,0,126.6,80.22h0C129.24,52.84,122.09,29.11,107.7,8.07ZM42.45,65.69C36.18,65.69,31,60,31,53s5-12.74,11.43-12.74S54,46,53.89,53,48.84,65.69,42.45,65.69Zm42.24,0C78.41,65.69,73.25,60,73.25,53s5-12.74,11.44-12.74S96.23,46,96.12,53,91.08,65.69,84.69,65.69Z"
-                />
+        <div class="discord-div2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 127.14 96.36"
+            class="clyde2"
+          >
+            <defs></defs>
+            <g id="图层_2" data-name="图层 2">
+              <g id="Discord_Logos" data-name="Discord Logos">
+                <g
+                  id="Discord_Logo_-_Large_-_White"
+                  data-name="Discord Logo - Large - White"
+                >
+                  <path
+                    class="cls-1"
+                    d="M107.7,8.07A105.15,105.15,0,0,0,81.47,0a72.06,72.06,0,0,0-3.36,6.83A97.68,97.68,0,0,0,49,6.83,72.37,72.37,0,0,0,45.64,0,105.89,105.89,0,0,0,19.39,8.09C2.79,32.65-1.71,56.6.54,80.21h0A105.73,105.73,0,0,0,32.71,96.36,77.7,77.7,0,0,0,39.6,85.25a68.42,68.42,0,0,1-10.85-5.18c.91-.66,1.8-1.34,2.66-2a75.57,75.57,0,0,0,64.32,0c.87.71,1.76,1.39,2.66,2a68.68,68.68,0,0,1-10.87,5.19,77,77,0,0,0,6.89,11.1A105.25,105.25,0,0,0,126.6,80.22h0C129.24,52.84,122.09,29.11,107.7,8.07ZM42.45,65.69C36.18,65.69,31,60,31,53s5-12.74,11.43-12.74S54,46,53.89,53,48.84,65.69,42.45,65.69Zm42.24,0C78.41,65.69,73.25,60,73.25,53s5-12.74,11.44-12.74S96.23,46,96.12,53,91.08,65.69,84.69,65.69Z"
+                  />
+                </g>
               </g>
             </g>
-          </g>
-        </svg>
-        <a class="discord-server2" href="https://discord.gg/jgMSBsTeSY" target="_blank">discord server</a>
+          </svg>
+          <a
+            class="discord-server2"
+            href="https://discord.gg/jgMSBsTeSY"
+            target="_blank"
+            >discord server</a
+          >
+        </div>
       </div>
+    </div>
 `;
