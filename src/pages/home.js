110f2db4e636ee4b2422e8ccb3b5d7207398d24d
noahwaseaten <kamen.mitsev@outlook.com>
2024-06-17 23:57:02 +0300

    made 414 px more reliable, and add discord server button

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,5 +3,6 @@ export const homeTemplate = () => `
     <h1 class="hi-noah">hi, I'm <span class="first-letter">n</span>oah</h1>
     <p>this is my "portfolio", which will be deleted permanently and remade ENTIRELY with React and made mobile friendly soon</p>
         <p>check out my <a href="https://github.com/noahwaseaten" target="_blank"> github </a> </p>
+        <div class="discord">
 </div>
 `;
