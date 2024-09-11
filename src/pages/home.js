5d5d85de545dbfb9bf6b3c8fab6651e91485704e
kamencho <personal@noahwaseaten.com>
2024-09-11 12:10:33 +0300

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,7 @@
 export const homeTemplate = () => `
 <div id="container">
     <h1 class="hi-noah">hi, I'm <span class="first-letter">n</span>oah</h1>
-    <p>this is my "portfolio", which will be deleted permanently and remade ENTIRELY with React and made mobile friendly soon</p>
+    <p>unmaintained site !!! dont expect anything soon.</p>
         <p>check out my <a href="https://github.com/noahwaseaten" target="_blank"> github </a> </p>
         <div class="discord">
 </div>
